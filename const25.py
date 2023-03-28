a223cf771c286eed0e1c5391fd7703fefbe0c08e
Dmitry <warfest.gg@mail.ru>
2023-03-28 15:07:25 +0300

    new 25

--- const25.py
+++ const25.py
@@ -1,11 +1,13 @@
-arr =[]
+arr, result = [], []
 
-arr1 = [2**x for x in range(2, 28, 2)]
-arr2 = [3**x for x in range(1, 25, 2)]
+arr1 = [[2**x, x] for x in range(28)]
+arr2 = [[3**x, x] for x in range(25)]
 
 for x in arr1:
     for y in arr2:
-        if 400_000_000 < x * y < 600_000_000:
-            arr.append(x * y)
+        if 400_000_000 < x[0] * y[0] < 600_000_000:
+            arr.append(f'2^{x[-1]} * 3^{y[-1]} == {x[0] * y[0]}')
+            result.append(x[0] * y[0])
 
 print(arr)
+print(f'result: {sorted(result)}')
