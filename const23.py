e413477645f39367f64528cb7b53e03e10574647
Dmitry <warfest.gg@mail.ru>
2023-03-28 14:47:50 +0300

    gg

--- const23.py
+++ const23.py
@@ -8,15 +8,3 @@ def func(x = 1, y = 21):
 print(func())
 
 
-arr =[]
-
-arr1 = [2**x for x in range(2, 28, 2)]
-arr2 = [3**x for x in range(1, 25, 2)]
-
-for x in arr1:
-    for y in arr2:
-        if 400_000_000 < x * y < 600_000_000:
-            arr.append(x * y)
-
-print(arr)
-
