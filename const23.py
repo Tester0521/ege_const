f4ffb6825cc4e2e9feac05fcdf23f8dffd43aea6
Dmitry <warfest.gg@mail.ru>
2023-03-30 13:34:59 +0300

    const23 updated

--- const23.py
+++ const23.py
@@ -8,3 +8,12 @@ def func(x = 1, y = 21):
 print(func())
 
 
+
+# на пути не должно быть 47 и должно быть 12
+
+def f(x, y):
+    if x > y or x == 47: return 0
+    if x == y: return 1
+    else: return f(x+1, y) + f(x*2, y)
+
+print(f(2, 12) * f(12, 50))
